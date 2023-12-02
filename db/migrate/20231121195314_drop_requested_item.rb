778d0fdb1f6d9dd26044fc03346d312dd15000b1
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-12-02 03:48:51 -0500

    More debug

--- db/migrate/20231121195314_drop_requested_item.rb
+++ db/migrate/20231121195314_drop_requested_item.rb
@@ -1,5 +1,5 @@
 class DropRequestedItem < ActiveRecord::Migration[6.1]
   def change
-    drop_table :food_requests
+    drop_table :food_requests if ActiveRecord::Base.connection.table_exists?('food_requests')
   end
 end
